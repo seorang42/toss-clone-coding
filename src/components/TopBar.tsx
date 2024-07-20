ab9ebc55bd555e0ba75c815cca7a830897145f62
seorang42 <seorang42@naver.com>
2024-07-20 19:22:16 +0900

    TopBar 반응형 레이아웃 구현

--- src/components/TopBar.tsx
+++ src/components/TopBar.tsx
@@ -1,7 +1,8 @@
 import Image from "next/image";
 import logo from "../../public/Toss_Logo_Primary.png";
 import { useMotionValueEvent, useScroll } from "framer-motion";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import { CloseIcon, MenuIcon } from "./icons";
 
 export default function TopBar() {
   const { scrollY } = useScroll();
@@ -14,34 +15,79 @@ export default function TopBar() {
     }
   });
 
+  const [isMenuOpened, setIsMenuOpened] = useState(false);
+
   return (
     <nav
       className={`w-full h-[60px] fixed left-0 top-0 z-50 flex justify-center bg-white ${
         isScrolled && "border-b border-[#E6E8EA]"
       }`}
     >
-      <div className="w-full h-[59px] max-w-[1140px] flex justify-center">
-        <div className="w-[92%] flex justify-between items-center">
+      <div className="w-full h-[59px] max-w-[1140px] flex justify-center relative">
+        <div className="sm:w-[92%] flex justify-between items-center max-sm:w-full max-sm:px-6">
           <a
             href="/"
-            className="flex shrink-0 items-center h-fit relative w-[68px] overflow-hidden"
+            className="flex shrink-0 items-center h-fit relative w-[68px] mr-8 overflow-hidden"
           >
             <Image src={logo} alt="로고" className="w-fit h-14 object-cover" />
           </a>
-          <div className="flex items-center">
-            <ul className="flex gap-4">
-              <li className="navbar-button">회사 소개</li>
-              <li className="navbar-button">고객센터</li>
-              <li className="navbar-button">자주 묻는 질문</li>
-              <li className="navbar-button">토스인증서</li>
-              <li className="navbar-button">채용</li>
-            </ul>
-            <div className="flex gap-2 items-center font-light">
-              <div className="ml-[52px] navbar-button">KOR</div>
-              <div className="w-[1px] h-[14px] bg-gray-300" />
-              <div className="navbar-button text-gray-300">ENG</div>
-            </div>
+          <div className="sm:hidden flex gap-3 items-center mt-[1px] -mr-[14px]">
+            <button className="w-[76px] h-[30px] mt-[2px] opacity-80 hover:opacity-100 rounded-[15px] flex justify-center items-center text-[11.5px] text-white bg-toss-blue">
+              앱 다운로드
+            </button>
+            <button
+              onClick={() => setIsMenuOpened((prev) => !prev)}
+              className="w-10 h-[26px] flex justify-center items-center focus:bg-gray-100"
+            >
+              {!isMenuOpened ? <MenuIcon /> : <CloseIcon />}
+            </button>
           </div>
+          <ul
+            className={`${
+              isMenuOpened ? "max-sm:flex" : "max-sm:hidden"
+            } max-sm:absolute max-sm:w-full max-sm:top-full max-sm:left-0 max-sm:flex-col flex items-center h-full`}
+          >
+            <li className="navbar-button-container">
+              <a href="/" className="navbar-button">
+                회사 소개
+              </a>
+            </li>
+            <li className="navbar-button-container">
+              <a href="/" className="navbar-button">
+                고객센터
+              </a>
+            </li>
+            <li className="navbar-button-container">
+              <a href="/" className="navbar-button">
+                자주 묻는 질문
+              </a>
+            </li>
+            <li className="navbar-button-container">
+              <a href="/" className="navbar-button">
+                토스인증서
+              </a>
+            </li>
+            <li className="navbar-button-container">
+              <a href="/" className="navbar-button">
+                채용
+              </a>
+            </li>
+            <ul className="flex max-sm:gap-0 items-center font-light max-sm:w-full bg-white">
+              <li className="sm:px-2 h-full flex items-center sm:pl-[52px] max-sm:hover:bg-gray-100">
+                <a href="/" className="navbar-language-button">
+                  KOR
+                </a>
+              </li>
+              <li className="mt-[1px] w-1 h-4 text-gray-300 text-[11px] font-medium">
+                |
+              </li>
+              <li className="sm:px-2 h-full flex items-center max-sm:hover:bg-gray-100">
+                <a href="/" className="navbar-language-button text-gray-300">
+                  ENG
+                </a>
+              </li>
+            </ul>
+          </ul>
         </div>
       </div>
     </nav>
--- src/components/icons.tsx
+++ src/components/icons.tsx
@@ -2607,3 +2607,35 @@ export function FindCashIcon() {
     </svg>
   );
 }
+
+export function MenuIcon() {
+  return (
+    <svg
+      className="w-6 h-6"
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 24 24"
+    >
+      <path
+        fill="#B0B8C1"
+        d="M4.118 6.2h16a1.2 1.2 0 100-2.4h-16a1.2 1.2 0 100 2.4m16 4.6h-16a1.2 1.2 0 100 2.4h16a1.2 1.2 0 100-2.4m0 7h-16a1.2 1.2 0 100 2.4h16a1.2 1.2 0 100-2.4"
+        fillRule="evenodd"
+      ></path>
+    </svg>
+  );
+}
+
+export function CloseIcon() {
+  return (
+    <svg
+      className="w-6 h-6"
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 24 24"
+    >
+      <path
+        fill="#B0B8C1"
+        fill-rule="evenodd"
+        d="M13.815 12l5.651-5.651a1.2 1.2 0 00-1.697-1.698l-5.651 5.652-5.652-5.652a1.201 1.201 0 00-1.697 1.698L10.421 12l-5.652 5.651a1.202 1.202 0 00.849 2.049c.307 0 .614-.117.848-.351l5.652-5.652 5.651 5.652a1.198 1.198 0 001.697 0 1.2 1.2 0 000-1.698L13.815 12z"
+      ></path>
+    </svg>
+  );
+}
