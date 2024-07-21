d22a0cec320cb866e7cd9e96ea559a9424d8c2c4
seorang42 <seorang42@naver.com>
2024-07-21 20:26:16 +0900

    Section2 컴포넌트 반응형 레이아웃 구현

--- src/components/Section2.tsx
+++ src/components/Section2.tsx
@@ -9,9 +9,9 @@ export default function Section2() {
 
   return (
     <div className="w-full overflow-hidden flex justify-center">
-      <div className="w-full h-[1687px] max-w-[1140px] flex justify-center">
+      <div className="w-full sm:h-[1687px] max-w-[1140px] flex justify-center max-sm:py-[100px]">
         <motion.div
-          className="w-[92%] h-full relative"
+          className="responsive-width h-full relative"
           initial="hidden"
           whileInView="visible"
           viewport={{ once: true, amount: 0.2 }}
@@ -21,21 +21,22 @@ export default function Section2() {
         >
           <motion.div
             variants={contentVariants}
-            className="mt-[250px] gap-[30px] flex flex-col"
+            className="sm:mt-[250px] gap-[30px] max-sm:gap-[10px] flex flex-col"
             transition={{ type: "tween", duration: 0.5 }}
           >
             <TitleBox
-              category="홈 · 소비"
+              category="홈  ·  소비"
               content={"내 돈 관리,\n지출부터 일정까지\n똑똑하게"}
             />
           </motion.div>
-          <div className="absolute w-full max-h-[1000px] h-full -z-10 top-[250px] overflow-hidden">
+          <div className="max-sm:relative max-sm:h-[587px] max-sm:flex max-sm:justify-center">
             <motion.div
               variants={contentVariants}
-              className="absolute w-[600px] right-0 -mr-28"
+              className="max-sm:relative absolute w-[600px] sm:-right-[104px] sm:top-[250px] shrink-0 -z-10 max-sm:h-full max-sm:w-fit"
               transition={{ type: "tween", duration: 0.5 }}
             >
               <img
+                className="phone-img"
                 src="https://static.toss.im/screens/iPhone12_Clay_Shadow.png"
                 alt="img1-1"
               />
@@ -45,28 +46,29 @@ export default function Section2() {
                 alt="img1-2"
               />
             </motion.div>
+            <motion.div
+              variants={contentVariants}
+              className="max-sm:relative absolute w-[600px] sm:-left-[104px] sm:top-[586px] shrink-0 max-sm:h-full max-sm:w-fit max-sm:-ml-[100px]"
+              transition={{ type: "tween", duration: 0.5 }}
+            >
+              <img
+                className="phone-img"
+                src="https://static.toss.im/screens/iPhone12_Clay_Shadow.png"
+                alt="img2-1"
+              />
+              <img
+                className="screen-img"
+                src="https://static.toss.im/assets/homepage/newtossim/section1_1_home_02.png"
+                alt="img2-2"
+              />
+            </motion.div>
           </div>
           <motion.div
             variants={contentVariants}
-            className="absolute w-[600px] -z-10 left-0 top-[586px] -ml-28"
+            className="sm:absolute sm:bottom-[331px] sm:right-[30px]"
             transition={{ type: "tween", duration: 0.5 }}
           >
-            <img
-              src="https://static.toss.im/screens/iPhone12_Clay_Shadow.png"
-              alt="img2-1"
-            />
-            <img
-              className="screen-img"
-              src="https://static.toss.im/assets/homepage/newtossim/section1_1_home_02.png"
-              alt="img2-2"
-            />
-          </motion.div>
-          <motion.div
-            variants={contentVariants}
-            className="absolute bottom-[331px] right-[30px]"
-            transition={{ type: "tween", duration: 0.5 }}
-          >
-            <h2 className="text-2xl font-medium leading-normal text-gray-700">
+            <h2 className="text-[23px] max-sm:text-[17px] font-medium leading-normal text-gray-700">
               {
                 "토스에 계좌와 카드를 연결해 보세요.\n계좌 잔액, 대출 · 투자 내역은 기본,\n일자별 소비와 수입까지 한 번에 볼 수 있어요."
               }
--- src/components/TitleBox.tsx
+++ src/components/TitleBox.tsx
@@ -6,8 +6,12 @@ interface ITitleBox {
 export default function TitleBox({ category, content }: ITitleBox) {
   return (
     <>
-      <h2 className="text-toss-blue font-semibold text-[28px]">{category}</h2>
-      <h2 className="text-5xl font-semibold leading-normal">{content}</h2>
+      <h2 className="text-toss-blue font-semibold text-[28px] max-sm:text-[18px]">
+        {category}
+      </h2>
+      <h2 className="text-[50px] font-bold leading-[1.4] max-sm:text-[28px]">
+        {content}
+      </h2>
     </>
   );
 }
