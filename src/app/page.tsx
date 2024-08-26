c96a9b12a029db3c53004d1b20b29c8afd866738
seorang42 <seorang42@naver.com>
2024-08-26 19:32:58 +0900

    모바일에서 새로고침 시 화면 최상단으로 이동하지 않는 오류 해결2

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -20,8 +20,10 @@ export default function Home() {
 
   // 페이지가 새로고침 될 때 이전에 위치하던 곳의 애니메이션 자동재생 방지
   useEffect(() => {
-    window.scrollTo(0, 0);
-    setIsLoaded(true);
+    setTimeout(() => {
+      window.scrollTo(0, 0);
+      setIsLoaded(true);
+    }, 20);
   }, []);
 
   return (
